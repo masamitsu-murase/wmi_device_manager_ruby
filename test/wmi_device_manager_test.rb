a069f4d8520556af576a04f2abd590177eccf474
Masamitsu MURASE <masamitsu.murase@gmail.com>
2019-10-13 23:09:42 +0900

    Add tests.

--- test/wmi_device_manager_test.rb
+++ test/wmi_device_manager_test.rb
@@ -1,7 +1,16 @@
 require_relative 'test_helper'
 
 class WmiDeviceManagerTest < Test::Unit::TestCase
-  def test_that_it_has_a_version_number
+  def test_version
     assert_not_nil WmiDeviceManager::VERSION
+  end
+
+  def test_pci0_device
+    wdm = WmiDeviceManager::DeviceManager.new
+    device = wdm.find{ |i| i.BiosDeviceName == "\\_SB.PCI0" }
+    assert_not_nil device
+    assert_equal "\\_SB.PCI0", device.BiosDeviceName
+    assert_equal "\\_SB.PCI0", device.Device_BiosDeviceName
+    assert_equal "\\_SB.PCI0", device.DEVPKEY_Device_BiosDeviceName
   end
 end
